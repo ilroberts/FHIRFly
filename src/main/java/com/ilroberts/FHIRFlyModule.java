f4246df425514437c2b426ca9e464e0c22649473
iwan roberts <ilroberts@gmail.com>
2017-10-21 11:41:19 +0100

    added core guice module

---
+++ src/main/java/com/ilroberts/FHIRFlyModule.java
@@ -0,0 +1,11 @@
+package com.ilroberts;
+
+import com.google.inject.AbstractModule;
+
+public class FHIRFlyModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
